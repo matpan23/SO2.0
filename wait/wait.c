7f5e14c0d0b5766e457e4a3e15dd6157f5bad279
matpan <matteo23baronissi@gmail.com>
2022-10-27 12:15:57 +0200

    WAIT

--- wait/wait.c
+++ wait/wait.c
@@ -1,41 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
 
-int main (void){
-    pid_t pid1,pid2;
+int main(void) {
+    int value, value2;
 
-    pid1= fork();
+    value = fork();
+    if(value < 0) return 0;
 
-    if(pid1<0){
-        fprintf(stderr,"errore nella system call fork");
-        exit(0);
-    }
-    else
-        if(pid1==0){
-            printf("il pid del primo figlio è: %d \n", getpid());
-            for(int i=1; i<=50;i++){
-                printf("%d\t",i);
-            }
+    if(value == 0) 
+        printf("Il pid del primo figlio: %d e il pid del padre: %d\n", getpid(), getppid());
+    else if(value > 0) {
+        value2 = fork();
+        if (value2 < 0) return 0;
+
+        if(value2 == 0) {
+            printf("La somma del pid del figlio e del pid del padre: %d\n", getppid() + getpid());
         }
-    else{
-        pid2= fork();
-        if(pid2<0){
-            fprintf(stderr,"errore nella system call fork");
-            exit(0);
+        else if (value2 > 0) {
+            printf("Terminazione del primo figlio con pid %d\n", wait(NULL));
+            printf("Terminazione del secondo figlio con pid %d\n", wait(NULL));
         }
-        else
-            if(pid2==0){
-                printf("\n\n il pid del padre è %d\n\n",getppid());
-                for(int j=101;j<=150;j++){
-                    printf("%d\t",j);
-                }
-            }
-            else{
-                printf("\n\nil pid del primo figlio terminato è : %d\n",waitpid(-1,NULL,0));
-                printf("\nil pid del secondo figlio terminato è : %d\n",waitpid(-1,NULL,0));
-            }
     }
-return 0;
 }
